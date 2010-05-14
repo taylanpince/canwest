d660b9a19147197eef89564c40071f265a73fe91
Taylan Pince <taylanpince@gmail.com>
2010-05-14 17:52:37 -0400

    Removed leftover debug code

--- build/canwest/shows/views.py
+++ build/canwest/shows/views.py
@@ -48,7 +48,6 @@ def home(request):
     """
     Renders the home page
     """
-    print request.META.get("HTTP_USER_AGENT")
     return render_to_response("home.html", {
         
     }, context_instance=RequestContext(request))
