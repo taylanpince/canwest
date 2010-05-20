b7044f5c9780675b10ab13434a7cd1ffad4a05b0
Taylan Pince <taylanpince@gmail.com>
2010-05-20 14:49:19 -0400

    Added support for properly sorting the days in the schedule

--- build/canwest/schedules/views.py
+++ build/canwest/schedules/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from admob.decorators import admob_analytics
-from schedules.models import ScheduledItem
+from schedules.models import ScheduledItem, DAY_CHOICES
 
 
 @admob_analytics
@@ -10,9 +10,11 @@ def list(request):
     """
     A list of all scheduled items, grouped by day
     """
+    days = dict(DAY_CHOICES).keys()
     scheduled_items = ScheduledItem.objects.all().order_by("day")
+    sorted_items = sorted(scheduled_items, key=lambda item: days.index(item.day))
 
     return render_to_response("schedules/list.html", {
-        "scheduled_items": scheduled_items,
+        "scheduled_items": sorted_items,
         "USE_GLOBAL_TEMPLATE": True,
     }, context_instance=RequestContext(request))
