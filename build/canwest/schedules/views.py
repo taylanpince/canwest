2dbc6fb474ec220b3295f7fffacfa722a05e821e
Taylan Pince <taylanpince@gmail.com>
2010-05-25 12:25:28 -0400

    Updated schedules to be ordered by start time as well as day

--- build/canwest/schedules/views.py
+++ build/canwest/schedules/views.py
@@ -11,7 +11,7 @@ def list(request):
     A list of all scheduled items, grouped by day
     """
     days = [day[0] for day in DAY_CHOICES]
-    scheduled_items = ScheduledItem.objects.all().order_by("day")
+    scheduled_items = ScheduledItem.objects.all().order_by("day", "start_time")
     sorted_items = sorted(scheduled_items, key=lambda item: days.index(item.day))
 
     return render_to_response("schedules/list.html", {
